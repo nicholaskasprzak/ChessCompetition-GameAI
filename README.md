85f3992eefa092ff1dd890dc67437647bd7adcac
Alexandre Tolstenko <tolstenko@gmail.com>
2024-01-25 12:22:25 -0500

    feat: chess gui

--- README.md
+++ README.md
@@ -8,18 +8,18 @@ You have to code a chess engine from the ground up. You will receive a string as
 
 ## Rules
 
-- Implement your chess engine only inside the chess-library folder in C++;
-- Create .h and .cpp files inside chess-library;
-- Obey the interface specified on chess-library;
+- Implement your chess engine only inside the chess-bot folder in C++;
+- Create .h and .cpp files inside chess-bot;
+- Obey the interface specified on chess-bot;
 - You might want to test your code via terminal via chess-cli, or chess-gui;
 - Merge requests are welcome;
-- When you submit your code, you should zip only the contents of the chess-library folder and send it to the system;
-- Do not use sub-folders inside the chess-library folder, it will break my automation;
+- When you submit your code, you should zip only the contents of the chess-bot folder and send it to the system;
+- Do not use sub-folders inside the chess-bot folder, it will break my automation;
 
 ## Folder structure
 
-- chess-library: Here you will implement your chess engine;
-- chess-cli: Here you will find the chess-cli code;
+- chess-bot: Here you will implement your chess engine;
+- chess-validator: Here you will find the chess-validator code;
 - chess-gui: Here you will find the chess-gui code;
 - frontend: Here you will find the web frontend code;
 - backend: Here you will find the web backend code;
@@ -30,7 +30,13 @@ You have to code a chess engine from the ground up. You will receive a string as
 - The tournament consists of making all players playing against each other on both sides of the board;
 - You may use up to 16GB ram and 12 cores of CPU;
 - Each turn should take less than 10 seconds;
-- You are not allowed to use any external library;
+- You are not allowed to use any external library besides the ones already provided here;
 - You can use AI assisted tools, but you have to state it on your code, and you will receive a penalty on your final score of 20%;
 - You may work in teams of up to 2 people;
 - Your username should not match your real name by any means in order do follow FERPA compliance;
+
+## DB structure
+
+- users: username, passhash
+- match: id, date, competitionId, whiteUserId, blackUserid, result, moves(uci), whiteScore, blackScore
+- competition: id, date, report(???), matches
--- chess-gui/main.cpp
+++ chess-gui/main.cpp
@@ -2,10 +2,52 @@
 #include "chess.hpp"
 
 #include "raylib.h"
-#include "raymath.h"
-
 #include "imgui.h"
 #include "rlImGui.h"
+#include "extras/IconsFontAwesome6.h"
+
+#include "PieceSvg.h"
+#include <map>
+
+void move(chess::Board& board) {
+    auto moveStr = ChessSimulator::Move(board.getFen(true));
+    auto move = chess::uci::uciToMove(board, moveStr);
+    board.makeMove(move);
+}
+
+auto SvgStringToTexture(std::string svgString){
+    auto document = lunasvg::Document::loadFromData(svgString);
+    auto bitmap = document->renderToBitmap(100, 100);
+    bitmap.convertToRGBA();
+    auto img = GenImageColor(100,100, WHITE);
+    img.data = bitmap.data();
+    auto tex = LoadTextureFromImage(img);
+    return tex;
+}
+
+auto loadPiecesTextures(){
+    std::map<char, Texture> map;
+
+    map['P'] = SvgStringToTexture(PawnWhiteSvgString);
+    map['p'] = SvgStringToTexture(PawnBlackSvgString);
+    map['R'] = SvgStringToTexture(RookWhiteSvgString);
+    map['r'] = SvgStringToTexture(RookBlackSvgString);
+    map['N'] = SvgStringToTexture(KnightWhiteSvgString);
+    map['n'] = SvgStringToTexture(KnightBlackSvgString);
+    map['B'] = SvgStringToTexture(BishopWhiteSvgString);
+    map['b'] = SvgStringToTexture(BishopBlackSvgString);
+    map['Q'] = SvgStringToTexture(QueenWhiteSvgString);
+    map['q'] = SvgStringToTexture(QueenBlackSvgString);
+    map['K'] = SvgStringToTexture(KingWhiteSvgString);
+    map['k'] = SvgStringToTexture(KingBlackSvgString);
+
+    return map;
+}
+
+enum class SimulationState {
+    PAUSED,
+    RUNNING,
+};
 
 int main(int argc, char* argv[])
 {
@@ -13,21 +55,28 @@ int main(int argc, char* argv[])
     //--------------------------------------------------------------------------------------
     int screenWidth = 1280;
     int screenHeight = 800;
-    int minScreenSide = 800;
-
+    int minScreenSide;
     SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
     InitWindow(screenWidth, screenHeight, "Chess AI Simulator");
     SetTargetFPS(144);
     rlImGuiSetup(true);
 
+    // ui settings
     bool aiEnabled = false;
-    chess::Color aiColor = chess::Color("b");
+    auto aiColor = chess::Color("w");
+    auto simulationState = SimulationState::PAUSED;
+
+    // load data
+    auto piecesTextures = loadPiecesTextures();
 
     chess::Board board;
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        if(simulationState == SimulationState::RUNNING)
+            move(board);
+
         BeginDrawing();
         ClearBackground(DARKGRAY);
 
@@ -37,52 +86,86 @@ int main(int argc, char* argv[])
         ImGui::Begin("Settings", nullptr);
         ImGui::Text("%.1fms %.0fFPS | AVG: %.2fms %.1fFPS", ImGui::GetIO().DeltaTime * 1000, 1.0f / ImGui::GetIO().DeltaTime,
                     1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
         ImGui::Separator();
-        if (ImGui::Button("Reset")) {
-//            state.Reset();
-//            score = Heuristics::MaterialScore(&state);
+        if (ImGui::Button(ICON_FA_PLAY)) {
+            simulationState = SimulationState::RUNNING;
         };
         ImGui::SameLine();
-        if (ImGui::Button("Undo")){
-//        && !previousStates.empty()) {
-//            validMoves = {};
-//            selected = {INT32_MIN, INT32_MIN};
-//            state = previousStates.top();
-//            previousStates.pop();
+        if (ImGui::Button(ICON_FA_PAUSE)) {
+            simulationState = SimulationState::PAUSED;
+        };
+        ImGui::SameLine();
+        if (ImGui::Button(ICON_FA_PLAY ICON_FA_PAUSE)){
+            simulationState = SimulationState::PAUSED;
+            move(board);
         }
         ImGui::Separator();
 
-//        ImGui::LabelText("Score", "%.1f", score);
-        ImGui::Separator();
-
-        if (ImGui::Checkbox("AI Enabled", &aiEnabled))
-            if (aiEnabled == true) aiColor = chess::Color("b");
-
-        static bool aiIsBlackStatic = true;
-        if (aiEnabled) {
-            if (ImGui::Checkbox("AI is Black", &aiIsBlackStatic)) {
-                if (aiIsBlackStatic)
-                    aiColor = chess::Color("b");
-                else
-                    aiColor = chess::Color("w");
-            }
-        }
-//        ImGui::LabelText(state.GetTurn() == PieceColor::White ? "White" : "Black", "Turn:");
         ImGui::End();  // end settings
 
         // game logic
 
-        // draww the chess board
+        // draw the chess board
         screenWidth = GetScreenWidth();
         screenHeight = GetScreenHeight();
         minScreenSide = screenWidth < screenHeight ? screenWidth : screenHeight;
 
+        auto displacementX = screenWidth/2 - minScreenSide/2;
+        auto displacementY = screenHeight/2 - minScreenSide/2;
+
+        Rectangle pieceRectSource {0,0,100,100};
+        Vector2 origin = {0,0};
+
         for(int row=0;row<8;row++){
             for(int col=0;col<8;col++){
+                Rectangle location = {displacementX + col*minScreenSide/8.f, screenHeight - displacementY - (row+1)*minScreenSide/8.f, minScreenSide/8.f, minScreenSide/8.f};
+
                 if((row+col)%2==0)
-                    DrawRectangle(screenWidth/2 - minScreenSide/2 + col*minScreenSide/8, row*minScreenSide/8, minScreenSide/8, minScreenSide/8, WHITE);
+                    DrawRectangleRec(location, WHITE);
                 else
-                    DrawRectangle(screenWidth/2 - minScreenSide/2 + col*minScreenSide/8, row*minScreenSide/8, minScreenSide/8, minScreenSide/8, BLACK);
+                    DrawRectangleRec(location, LIGHTGRAY);
+
+                switch(board.at(chess::Square(row*8+col)).internal()){
+                    case chess::Piece::underlying::WHITEPAWN:
+                        DrawTexturePro(piecesTextures['P'], pieceRectSource, location, origin, 0, WHITE);
+                        break;
+                    case chess::Piece::underlying::BLACKPAWN:
+                        DrawTexturePro(piecesTextures['p'], pieceRectSource, location, origin, 0, WHITE);
+                        break;
+                    case chess::Piece::underlying::WHITEROOK:
+                        DrawTexturePro(piecesTextures['R'], pieceRectSource, location, origin, 0, WHITE);
+                        break;
+                    case chess::Piece::underlying::BLACKROOK:
+                        DrawTexturePro(piecesTextures['r'], pieceRectSource, location, origin, 0, WHITE);
+                        break;
+                    case chess::Piece::underlying::WHITEKNIGHT:
+                        DrawTexturePro(piecesTextures['N'], pieceRectSource, location, origin, 0, WHITE);
+                        break;
+                    case chess::Piece::underlying::BLACKKNIGHT:
+                        DrawTexturePro(piecesTextures['n'], pieceRectSource, location, origin, 0, WHITE);
+                        break;
+                    case chess::Piece::underlying::WHITEBISHOP:
+                        DrawTexturePro(piecesTextures['B'], pieceRectSource, location, origin, 0, WHITE);
+                        break;
+                    case chess::Piece::underlying::BLACKBISHOP:
+                        DrawTexturePro(piecesTextures['b'], pieceRectSource, location, origin, 0, WHITE);
+                        break;
+                    case chess::Piece::underlying::WHITEQUEEN:
+                        DrawTexturePro(piecesTextures['Q'], pieceRectSource, location, origin, 0, WHITE);
+                        break;
+                    case chess::Piece::underlying::BLACKQUEEN:
+                        DrawTexturePro(piecesTextures['q'], pieceRectSource, location, origin, 0, WHITE);
+                        break;
+                    case chess::Piece::underlying::WHITEKING:
+                        DrawTexturePro(piecesTextures['K'], pieceRectSource, location, origin, 0, WHITE);
+                        break;
+                    case chess::Piece::underlying::BLACKKING:
+                        DrawTexturePro(piecesTextures['k'], pieceRectSource, location, origin, 0, WHITE);
+                        break;
+                    default:
+                        break;
+                }
             }
         }
 
