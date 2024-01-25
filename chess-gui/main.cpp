4b698b2143d2e710baefb3a9a47521275b475c56
Alexandre Tolstenko <tolstenko@gmail.com>
2024-01-25 00:24:23 -0500

    feat: chess simulator

--- chess-gui/main.cpp
+++ chess-gui/main.cpp
@@ -1,15 +1,5 @@
-/*******************************************************************************************
-*
-*   raylib-extras [ImGui] example - Simple Integration
-*
-*	This is a simple ImGui Integration
-*	It is done using C++ but with C style code
-*	It can be done in C as well if you use the C ImGui wrapper
-*	https://github.com/cimgui/cimgui
-*
-*   Copyright (c) 2021 Jeffery Myers
-*
-********************************************************************************************/
+#include "chess-simulator.h"
+#include "chess.hpp"
 
 #include "raylib.h"
 #include "raymath.h"
@@ -23,11 +13,17 @@ int main(int argc, char* argv[])
     //--------------------------------------------------------------------------------------
     int screenWidth = 1280;
     int screenHeight = 800;
+    int minScreenSide = 800;
 
     SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "raylib-Extras [ImGui] example - simple ImGui Demo");
+    InitWindow(screenWidth, screenHeight, "Chess AI Simulator");
     SetTargetFPS(144);
     rlImGuiSetup(true);
+
+    bool aiEnabled = false;
+    chess::Color aiColor = chess::Color("b");
+
+    chess::Board board;
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -38,9 +34,59 @@ int main(int argc, char* argv[])
         // start ImGui Conent
         rlImGuiBegin();
 
-        // show ImGui Content
-        bool open = true;
-        ImGui::ShowDemoWindow(&open);
+        ImGui::Begin("Settings", nullptr);
+        ImGui::Text("%.1fms %.0fFPS | AVG: %.2fms %.1fFPS", ImGui::GetIO().DeltaTime * 1000, 1.0f / ImGui::GetIO().DeltaTime,
+                    1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::Separator();
+        if (ImGui::Button("Reset")) {
+//            state.Reset();
+//            score = Heuristics::MaterialScore(&state);
+        };
+        ImGui::SameLine();
+        if (ImGui::Button("Undo")){
+//        && !previousStates.empty()) {
+//            validMoves = {};
+//            selected = {INT32_MIN, INT32_MIN};
+//            state = previousStates.top();
+//            previousStates.pop();
+        }
+        ImGui::Separator();
+
+//        ImGui::LabelText("Score", "%.1f", score);
+        ImGui::Separator();
+
+        if (ImGui::Checkbox("AI Enabled", &aiEnabled))
+            if (aiEnabled == true) aiColor = chess::Color("b");
+
+        static bool aiIsBlackStatic = true;
+        if (aiEnabled) {
+            if (ImGui::Checkbox("AI is Black", &aiIsBlackStatic)) {
+                if (aiIsBlackStatic)
+                    aiColor = chess::Color("b");
+                else
+                    aiColor = chess::Color("w");
+            }
+        }
+//        ImGui::LabelText(state.GetTurn() == PieceColor::White ? "White" : "Black", "Turn:");
+        ImGui::End();  // end settings
+
+        // game logic
+
+        // draww the chess board
+        screenWidth = GetScreenWidth();
+        screenHeight = GetScreenHeight();
+        minScreenSide = screenWidth < screenHeight ? screenWidth : screenHeight;
+
+        for(int row=0;row<8;row++){
+            for(int col=0;col<8;col++){
+                if((row+col)%2==0)
+                    DrawRectangle(screenWidth/2 - minScreenSide/2 + col*minScreenSide/8, row*minScreenSide/8, minScreenSide/8, minScreenSide/8, WHITE);
+                else
+                    DrawRectangle(screenWidth/2 - minScreenSide/2 + col*minScreenSide/8, row*minScreenSide/8, minScreenSide/8, minScreenSide/8, BLACK);
+            }
+        }
+
+        // end game logic
 
         // end ImGui Content
         rlImGuiEnd();
